4c25847bdb41473111bee221f9444f7d5c85bece
teerayutgrit <guywork08@gmail.com>
2024-05-27 13:50:00 +0700

    initial commit

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<div class="row ">
+      <div class="col-6 justify-content-center">
+        <a class="btn btn-primary" href="dbcon.php" role="button">ข้อมูลสรุปรายวัน</a>
+      </div>
+    </div>
+</body>
+</html>
